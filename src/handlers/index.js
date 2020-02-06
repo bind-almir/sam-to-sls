f9c640185a2c9cf51e743cfa0b6638b1b50e260f
Almir Zulic <almir@bind.ba>
2020-02-06 13:30:59 +0100

    setup

---
+++ src/handlers/index.js
@@ -0,0 +1,12 @@
+const request = require('request');
+// unused dependency could be removed or is it a part of the test?
+
+exports.handler = async event => {
+  console.log('event', event);
+  console.log(`client_id: ${process.env.client_id}`);
+  console.log(`client_secret: ${process.env.client_secret}`);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  }
+};
